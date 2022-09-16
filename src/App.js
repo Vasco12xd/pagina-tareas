6174a89f233ca2a42dd25aadcf7c9ad52a9465c3
Esteban Vasco <evasco.27@hotmail.com>
2022-09-16 07:35:20 -0500

    Funcionalida search
    Se hace la filtración de los TODOs que se encuentran con las mismas letras buscadas

--- src/App.js
+++ src/App.js
@@ -6,31 +6,57 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
-const todos=[
-  {text:'Cortar cebolla', completed:false},
+const defaultTodos=[
+  {text:'Cortar cebolla', completed:true},
   {text:'Tormar el curso de intro a react', completed:false},
   {text:'Llorar con la llorona', completed:false},
   {text: 'LALALALAA', completed: false },
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos);
+  // El estado de nuestra búsqueda
+  const [searchValue, setSearchValue ] = React.useState('');
+
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+  // Creamos una nueva variable en donde guardaremos las coincidencias con la búsqueda
+  let searchedTodos = [];
+  // Lógica para filtrar
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase()
+      const searchText = searchValue.toLowerCase()
+      return todoText.includes(searchText)
+    });
+  }
+
   return (
     <React.Fragment>
-    <TodoCounter />
-        
-    <TodoSearch />
-    <CreateTodoButton />
+    <TodoCounter 
+      total={totalTodos}
+      completed={completedTodos}
+    />
+    <TodoSearch 
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    />
+    
 
     <TodoList>
-        {todos.map(todo =>(
+        {searchedTodos.map(todo =>(
         <TodoItem 
         key={todo.text} 
         text={todo.text} 
         completed={todo.completed}
-        />))}
+        />
+        ))}
       </TodoList>
     
-    
+      <CreateTodoButton />
     </React.Fragment>
   );
 }
