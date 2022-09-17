8646f7921b73ffe6e8e824897d8f6a85e2b6949d
Esteban Vasco <evasco.27@hotmail.com>
2022-09-16 21:49:24 -0500

    Organizacion de archivos y funciones

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/index.js';
 import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
