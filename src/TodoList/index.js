f0704f52ea2b8074c99c20fe6b3f3cf130e7b877
Esteban Vasco <evasco.27@hotmail.com>
2023-04-25 13:19:45 -0500

    identando

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import './TodoList.css';
 
 function TodoList(props){
-    return(
-        <section className="TodoList-container">
+    return (
+      <section className="TodoList-container">
+        {props.error && props.onError()}
+        {props.loading && props.onLoading()}
+        {!props.loading && !props.totalTodos && props.onEmptyTodos()}
+        {/* {props.searchedTodos.map(props.render)} */}
+        {props.searchedTodos.map(props.render || props.children)}
+        {!!props.totalTodos &&
+          !props.searchedTodos.length &&
+          props.onEmptySearchResult(props.searchText)}
 
-            {props.error && props.onError()}
-            {props.loading && props.onLoading()}
-            {(!props.loading && !props.totalTodos) && props.onEmptyTodos()}
-            {/* {props.searchedTodos.map(props.render)} */}
-            {props.searchedTodos.map(props.render || props.children)}
-            {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResult(props.searchText)}
-
-            <ul>
-                {props.children}
-            </ul>
-        </section>
+        <ul>{props.children}</ul>
+      </section>
     );
 }
 
